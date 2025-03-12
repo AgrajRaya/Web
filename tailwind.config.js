ebe30076de6e99bde2b6d0f4d2a092dd9e499b08
unknown <ayushrayamajhi75@gmail.com>
2025-03-11 20:12:39 -0400

    removed the miscellaneous lines

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
+ 
     content: [`./views/**/*.ejs`], // all .html files
-=======
+ 
     content: [`./views/**/*.html`], // all .html files
->>>>>>> 057687b9cf5baaac1a5916517d5ebba1947ec620
-    theme: {
+     theme: {
       extend: {},
     },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
     daisyui: {
-<<<<<<< HEAD
+
       themes: ["autumn"], 
-=======
+
       themes: ["fantasy"], 
->>>>>>> 057687b9cf5baaac1a5916517d5ebba1947ec620
     },
   };
   
